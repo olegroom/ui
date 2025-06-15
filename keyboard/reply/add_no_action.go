6d260a24fe5cd7004aa9d6abfd16ff12a7c5a8e7
rumyancev.oleg6 <rumyancev.oleg6@wb.ru>
2025-06-15 15:29:30 +0300

    Убрал ненужные переменные из функции ButtonNoAction

--- keyboard/reply/add_no_action.go
+++ keyboard/reply/add_no_action.go
@@ -1,11 +1,10 @@
 package reply
 
 import (
-	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
 )
 
-func (kb *ReplyKeyboard) ButtonNoAction(text string, b *bot.Bot, matchType bot.MatchType) *ReplyKeyboard {
+func (kb *ReplyKeyboard) ButtonNoAction(text string) *ReplyKeyboard {
 	kb.markup[len(kb.markup)-1] = append(kb.markup[len(kb.markup)-1], models.KeyboardButton{
 		Text: text,
 	})
