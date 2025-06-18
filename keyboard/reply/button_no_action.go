63c5896f4384fdc86fc3faed6b253d7b20ecbc14
rumyancev.oleg6 <rumyancev.oleg6@wb.ru>
2025-06-18 18:37:20 +0300

    new tag

--- keyboard/reply/button_no_action.go
+++ keyboard/reply/button_no_action.go
@@ -11,3 +11,12 @@ func (kb *ReplyKeyboard) ButtonNoAction(text string) *ReplyKeyboard {
 
 	return kb
 }
+
+func (kb *ReplyKeyboard) ButtonNoActionWithRequestContacts(text string) *ReplyKeyboard {
+	kb.markup[len(kb.markup)-1] = append(kb.markup[len(kb.markup)-1], models.KeyboardButton{
+		Text:           text,
+		RequestContact: true,
+	})
+
+	return kb
+}
